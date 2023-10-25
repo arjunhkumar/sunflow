4d786cc92f804c889b66cb3fa0aaf8b0890e55a0
arjunhkumar <s21008@students.iitmandi.ac.in>
2023-10-25 21:25:41 +0530

    Pipelining changes.

---
+++ build-1.8.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+rm -r artifacts/
+ant -f build-1.8.xml clean
+ant -f build-1.8.xml
+mkdir -p artifacts/jar
+cp release/sunflow.jar artifacts/jar/sunflow-0.07.2.jar
+cp release/janino.jar artifacts/jar/janino-2.5.15.jar
+cp sanity/dacapo-9.12-MR1-bach.jar artifacts/dacapo-modified.jar
+cp sanity/dacapo-9.12-MR1-bach.jar artifacts/dacapo-modified.jar
+cp sanity/poa.jar artifacts/poa.jar
+cd artifacts/
+jar -uf dacapo-modified.jar jar/janino-2.5.15.jar
+jar -uf dacapo-modified.jar jar/sunflow-0.07.2.jar
+/usr/lib/jvm/java-8-openjdk-amd64/bin/java -javaagent:poa.jar -jar dacapo-modified.jar sunflow -n 100
